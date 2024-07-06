549d1a72f1f0dc70c696785d04132a94bdffaefa
Théo Laperrouse <theo.laperrouse@energiency.com>
2024-07-06 14:53:15 +0200

    feat: add priority label and resolve new line bug

--- src/composables/useDices.js
+++ src/composables/useDices.js
@@ -1,23 +1,43 @@
 import { ref } from 'vue';
 
+const priorityLabels = [
+    'Double 6, distribue 6 gorgées et invente une règle',
+    'Dernier à dire "BIZKIT" boit',
+    'Double BIZKIT',
+    'Le lanceur devient la Noisette',
+];
+
 export function useDices(numberOfDice = 2) {
     const dices = ref(Array(numberOfDice).fill(1));
+    const actionLabel = ref('');
 
     const rollDices = () => {
         for (let i = 0; i < dices.value.length; i++) {
             dices.value[i] = Math.floor(Math.random() * 6) + 1;
         }
-        actionLabel.value = getActionLabel(dices.value[0], dices.value[1]);
-        if (dices.value.length === 4) {
-            actionLabel.value += `</br>${getActionLabel(dices.value[2], dices.value[3])}`;
+        const firstActionLabel = getActionLabel(dices.value[0], dices.value[1]);
+        const secondActionLabel = getActionLabel(dices.value[2], dices.value[3]);
+
+        const priorityLabel = [firstActionLabel, secondActionLabel].find((label) =>
+            priorityLabels.some((priority) => label === priority),
+        );
+        if (priorityLabel) {
+            actionLabel.value = priorityLabel;
+        } else if (firstActionLabel && secondActionLabel) {
+            actionLabel.value = `${firstActionLabel}</br>${secondActionLabel}`;
+        } else {
+            actionLabel.value = firstActionLabel ?? secondActionLabel ?? '';
         }
     };
 
     const getActionLabel = (dice1, dice2) => {
         const total = dice1 + dice2;
+
         switch (total) {
             case 2:
                 return 'Le lanceur boit';
+            case 3:
+                return 'Le lanceur devient la Noisette';
             case 6:
                 return 'Le précédent boit';
             case 7:
@@ -27,17 +47,15 @@ export function useDices(numberOfDice = 2) {
             case 12:
                 return 'Double 6, distribue 6 gorgées et invente une règle';
             default:
-                if (dice1 === dice2) {
+                if (dice1 && dice1 === dice2) {
                     return `Double ${dice1}, distribue ${dice1} gorgée(s)`;
                 }
                 if (dice1 === 3 || dice2 === 3) {
                     return 'La Noisette boit';
                 }
-                return '';
+                return null;
         }
     };
-
-    const actionLabel = ref('');
 
     return { dices, rollDices, actionLabel };
 }
